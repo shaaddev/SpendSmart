fe221be7d4ba4f0d54759d163422564c26da6af5
Rashaad Lee Hue <rashaadleehue1@gmail.com>
2024-07-16 23:47:02 -0400

    fix: responsive design for the main page

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -2,7 +2,7 @@ import { HomeBtn } from "./home-btn"
 
 export function HomePage(){
   return(
-    <div className="min-h-96 flex flex-col lg:grid  justify-center text-center items-center w-full">
+    <div className="min-h-96 mb-36 lg:mb-0 flex flex-col lg:grid  justify-center text-center items-center w-full">
       <div>
         <h1 className="text-3xl md:text-6xl font-bold mb-5">Take Control of Your Finances with SpendSmart</h1>
         <p className="mb-5">SpendSmart is the ultimate finance tracking app that helps you manage your budget, track expenses, and gain insights into your
